ad94c2008ec64c00cb25c13ffca265685ae30c1a
Yajat Vishwakk <yajat472001@gmail.com>
2019-11-19 01:44:05 +0530

    Add files via upload

--- zippp.py
+++ zippp.py
@@ -4,11 +4,12 @@
 # in code manually
 # 2. the zip file can contain whatever files, the scropt will extract and read its content
 # 3. the final content of zip file will b written in a txt file called 'final.txt'
-# 4. A cup of coffee for making me do this will be appreciated eshan... 
+# 4. A cup of coffee for making me do this will be appretiated eshan... 
 # Written on : 15th Nov, 2019
-
+# Modified: 18th Nov, 2019 - added Showing only redundant data
 from zipfile import ZipFile
 filename ='zipp.zip'
+
 with ZipFile(filename, 'r') as zip:
     zip.printdir()
     data = zip.namelist()
@@ -18,8 +19,20 @@ with ZipFile(filename, 'r') as zip:
 con = []
 for fil in data:
     with open(fil,'r') as f:
-            con.append(f.read())
+        ele = f.read()
+        con.append(ele)
+con2 = []
+for ele in con:
+    for element in ele.split('\n'):
+        con2.append(element)
+d = {x:con2.count(x) for x in con2}
+final =[]
+for key, value in d.items():
+    if(value == 3):
+        final.append(key)
+
 with open('final.txt', 'w') as f:
-    for i in con: 
+    for i in final: 
         f.write(i+' ')
+    
 print('done')
